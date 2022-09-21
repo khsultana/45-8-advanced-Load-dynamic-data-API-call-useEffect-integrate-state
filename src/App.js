266811609f5f2fee796366dfecafbe58b4c8e038
Khan Sultana <Khan.Sultana@fifo-tech.com>
2022-09-21 16:08:30 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <External></External>
     </div>
   );
 }
@@ -14,7 +15,6 @@ function Counter() {
   const [count, setCount] = useState(0)
   const increaseCount = () => setCount(count + 1);
   const decreaseCount = () => setCount(count - 1)
-
   return (
     <div>
       <h2>Count:{count}</h2>
@@ -24,4 +24,36 @@ function Counter() {
   )
 }
 
+
+function External() {
+  const [users, setUsers] = useState([])
+  // useEffect(() => { }, [])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+
+
+  return (
+    <div>
+      <h2>External</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <SimpleUser name={user.name} email={user.email}></SimpleUser>)
+      }
+    </div>
+  )
+}
+
+
+function SimpleUser(props) {
+  return (
+    <div style={{ border: "2px solid red", margin: '20px' }}>
+      <h3>Name: {props.name}</h3>
+      <p>Email : {props.email}</p>
+    </div>
+  )
+}
 export default App;
